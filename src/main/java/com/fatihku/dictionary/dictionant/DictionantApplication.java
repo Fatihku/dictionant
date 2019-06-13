8980efebd55b298d29392f394ce52666850efbf3
Fatih Kuyucuoglu <fatihku@gmail.com>
2019-06-13 20:25:40 +0200

    the initial commit

---
+++ src/main/java/com/fatihku/dictionary/dictionant/DictionantApplication.java
@@ -0,0 +1,13 @@
+package com.fatihku.dictionary.dictionant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionantApplication.class, args);
+	}
+
+}
